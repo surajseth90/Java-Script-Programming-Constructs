89b8344d11eeced0cd2db15defaf1ca8c180845b
surajseth90 <surajseth90@gmail.com>
2021-05-25 10:56:44 +0530

    Added JavaScipt code for Printing Number Units by name

---
+++ PrintNumberUnits.js
@@ -0,0 +1,13 @@
+// Printing Number Units bye name
+
+let digit = process.argv;
+if(digit==1)
+    console.log("Unit");
+else if(digit==10)
+    console.log("Ten");
+else if(digit==100)
+    console.log("Hundred");
+else if(digit==1000)
+    console.log("Thousand");
+else
+    console.log("Invalid Number!!")
