d7abea429c46c957caee173069e54d0be317f9dc
surajseth90 <surajseth90@gmail.com>
2021-05-25 11:10:36 +0530

    Added JavaScipt code to Prinit Single Digit Number By Thier Word name using switch cases

---
+++ SwitchCaseProblem1.js
@@ -0,0 +1,38 @@
+// Prinitng Single Digit Number By Thier Word name using switch cases
+
+let digit = Math.floor(Math.random()*10);
+console.log("value is : " + digit)
+switch(digit){
+    case 0:
+        console.log("Zero"); 
+        break;
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+    case 4:
+        console.log("Four");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+    case 6:
+        console.log("Six");
+        break;
+    case 7:
+        console.log("Seven");
+        break;
+    case 8:
+        console.log("Eight");
+        break;
+    case 9:
+        console.log("Nine");
+        break;  
+    default:
+        console.log("Invalid Input!!");
+}
