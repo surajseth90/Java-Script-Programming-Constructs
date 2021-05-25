b5db9603ec00f55bd0e050ab49301d6f7aaea4f9
surajseth90 <surajseth90@gmail.com>
2021-05-25 10:40:59 +0530

    Added JavaScipt code to find days of a week with respect to iniger values

---
+++ PrintingDaysOfWeek.js
@@ -0,0 +1,18 @@
+// Finding days of a week with respect to iniger values
+
+let day = Math.floor(Math.random()*10)%7+1;
+console.log(day)
+if(day==1)
+    console.log("Monday");
+else if(day==2)
+    console.log("Tuesday");
+else if(day==3)
+    console.log("Wednesday");
+else if(day==4)
+    console.log("Thursday");
+else if(day==5)
+    console.log("Friday");
+else if(day==6)
+    console.log("Saturday");
+else if(day==7)
+    console.log("Sunday");
